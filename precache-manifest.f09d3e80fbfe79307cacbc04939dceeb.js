56bd5ac97f03ddd8a2bdeb8b3f3180a45013a6b5
Sunil561 <gupta.sunil561@gmail.com>
2020-09-13 19:19:17 +0530

    Updates

---
+++ precache-manifest.f09d3e80fbfe79307cacbc04939dceeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3579a27e561fb94e161e1e9a58a28a",
+    "url": "/FirstWebsite/index.html"
+  },
+  {
+    "revision": "4d06d7157d9976c63e22",
+    "url": "/FirstWebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39e737da7e45bd035321",
+    "url": "/FirstWebsite/static/js/2.5f0638a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FirstWebsite/static/js/2.5f0638a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d06d7157d9976c63e22",
+    "url": "/FirstWebsite/static/js/main.38688709.chunk.js"
+  },
+  {
+    "revision": "c88a53a2a7169ba622a5",
+    "url": "/FirstWebsite/static/js/runtime-main.d514e41e.js"
+  }
+]);
